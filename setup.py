e31f392cac5cf19ab582b69184f6a46031498e34
Yaroslav Polyakov <yaroslaff@gmail.com>
2020-01-17 19:49:23 +0700

    contrib added to manifest

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 
 setup(name='okerrupdate',
-    version='1.2.1',
+    version='1.2.2',
     description='client-side okerr module and utilities',
     url='https://gitlab.com/yaroslaff/okerrupdate',
     author='Yaroslav Polyakov',
